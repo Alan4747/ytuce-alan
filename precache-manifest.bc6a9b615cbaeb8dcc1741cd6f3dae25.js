57709b029cd5e4074846a77d85a8aaf5f62046c7
alan4747 <alan47m@gmail.com>
2019-10-10 23:23:47 +0300

    Updates

---
+++ precache-manifest.bc6a9b615cbaeb8dcc1741cd6f3dae25.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b147bf336c65e9aed675fb8e2e7665",
+    "url": "/ytuce-alan/index.html"
+  },
+  {
+    "revision": "87b17584fd9c22fb8805",
+    "url": "/ytuce-alan/static/css/2.dd5c51eb.chunk.css"
+  },
+  {
+    "revision": "ee206c20fb9b88b0bda2",
+    "url": "/ytuce-alan/static/css/main.c22b8042.chunk.css"
+  },
+  {
+    "revision": "87b17584fd9c22fb8805",
+    "url": "/ytuce-alan/static/js/2.b443dacc.chunk.js"
+  },
+  {
+    "revision": "ee206c20fb9b88b0bda2",
+    "url": "/ytuce-alan/static/js/main.9d1f9c4f.chunk.js"
+  },
+  {
+    "revision": "de967c289d99f4775935",
+    "url": "/ytuce-alan/static/js/runtime~main.915f8264.js"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/ytuce-alan/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/ytuce-alan/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/ytuce-alan/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/ytuce-alan/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "9dfd56d0a4805caf89c653b3f8d16874",
+    "url": "/ytuce-alan/static/media/yildiz-logo.9dfd56d0.svg"
+  }
+]);
